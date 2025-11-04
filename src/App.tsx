d1a39dc398bdb25b70007a21b3cda9abd85e4f6b
Brian H <b.r.hendrick@gmail.com>
2025-11-04 15:36:31 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient();
 
 // GitHub Pages basename configuration
 const basename = import.meta.env.MODE === 'production'
-  ? '/iis-portal-prototype/'
+  ? '/iis-portal-prototype-v1/'
   : '/';
 
 const App = () => (
